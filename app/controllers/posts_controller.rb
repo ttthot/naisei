0ad6476f0af6ef36cbcb30730890ee053a0523ca
ttthot <68436657+ttthot@users.noreply.github.com>
2025-04-20 02:45:40 +0000

    Add configuration files for Ruby development environment and update PostsController
    - Create .rubocop.yml for RuboCop configuration
    - Create .vscode/extensions.json and .vscode/launch.json for VSCode settings
    - Update Gemfile and Gemfile.lock to include ruby-lsp and related gems
    - Enhance PostsController with index, new, and create actions
    - Modify index.html.erb to update header text
    - Add gomi.erb as an example of incorrect indentation
    - Add frozen_string_literal comment to migration file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,32 +2,34 @@
 
 # 記事を取得する
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def new
-        @post = Post.new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-        # HTTPリクエストで送られてきたパラメータから、安全に選別された属性だけを使って新しいPostオブジェクトを初期化する処理
-        @post = Post.new(post_params)
-        if @post.save
-            # posts_pathは、postsコントローラのindexアクションに対応するURLを生成するヘルパーメソッド
-            # redirect_toメソッドは、HTTPレスポンスを302リダイレクトに設定し、指定されたURLにリダイレクトする
-            # noticeは、リダイレクト先のページで表示されるフラッシュメッセージを指定するオプション
-            # フラッシュメッセージは、リダイレクト先のページで一度だけ表示されるメッセージ
-            redirect_to posts_path, notice: '記事を作成しました'
-        else
-            render :new
-        end
+  def create
+    # HTTPリクエストで送られてきたパラメータから、安全に選別された属性だけを使って新しいPostオブジェクトを初期化する処理
+    @post = Post.new(post_params)
+    if @post.save
+      # posts_pathは、postsコントローラのindexアクションに対応するURLを生成するヘルパーメソッド
+      # redirect_toメソッドは、HTTPレスポンスを302リダイレクトに設定し、指定されたURLにリダイレクトする
+      # noticeは、リダイレクト先のページで表示されるフラッシュメッセージを指定するオプション
+      # フラッシュメッセージは、リダイレクト先のページで一度だけ表示されるメッセージ
+      redirect_to posts_path, notice: '記事を作成しました'
+    else
+      render :new
     end
+  end
 
-    def post_params
-        # ストロングパラメータを使用して、許可された属性だけを取得する
-        # :postは、HTTPリクエストのボディに含まれるパラメータの名前
-        # form_with(model: @post) を使うと、Railsは各入力フィールドのname属性を自動的に "post[title]" や "post[content]" のように設定します。
-        params.require(:post).permit(:title, :content)
-    end
+  private
+
+  def post_params
+    # ストロングパラメータを使用して、許可された属性だけを取得する
+    # :postは、HTTPリクエストのボディに含まれるパラメータの名前
+    # form_with(model: @post) を使うと、Railsは各入力フィールドのname属性を自動的に "post[title]" や "post[content]" のように設定します。
+    params.require(:post).permit(:title, :content)
+  end
 end
--- db/migrate/20250418070913_create_topics.rb
+++ db/migrate/20250418070913_create_topics.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class CreateTopics < ActiveRecord::Migration[7.1]
   def change
     create_table :topics do |t|
