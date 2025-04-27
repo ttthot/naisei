216b4720162ef9a3165cb3eb378c97afa225241a
ttthot <softbasoftb@gmail.com>
2025-04-27 14:45:52 +0000

    エラーハンドリング途中まで5228

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  # get '/login'のルーティングに対応するアクション
   def new
+    # @session = Session.newはモデルを使用しないため不要
+  end
+
+  # post '/login'のルーティングに対応するアクション
+  def create
+    # emailカラムがparams[:session][:email]の値と一致するユーザーを検索しています。
+    user = User.find_by(email: params[:session][:email].downcase)
+    # @userがnilでない場合、つまりユーザーが存在する場合
+    if user&.authenticate(params[:session][:email])
+      puts 'ログイン成功'
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,22 +10,27 @@
 # ルートパス（"/"）はデフォルトではコメントアウトされています。
 # デフォルトのランディングページを定義するには、`root` の行をアンコメントして修正してください。
 Rails.application.routes.draw do
-  get 'sessions/new'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
+
+  get '/signup', to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
 
   # localhost:3000/posts
   # localhost:3000/posts/new
-  resources :posts, only: [:index, :new, :create] 
-  resources :users, only: [:index, :show, :new, :create]
+  resources :posts, only: %i[index new create]
+  resources :users, only: %i[index show new create]
 end
