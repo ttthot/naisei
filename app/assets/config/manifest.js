1b791b34954f527f382ce0c6ba88999c39bfa488
ttthot <softbasoftb@gmail.com>
2025-04-29 12:24:53 +0000

    tailwind本体インストール完了

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,14 @@
+/**
+ * このファイルは、アセットパイプラインの設定ファイルです。
+ * Railsアプリケーションで使用される静的アセット（画像、スタイルシート、JavaScriptなど）の
+ * パスを指定し、リンクを作成するために使用されます。
+ *
+ * @file Manages the asset pipeline configuration for a Rails application.
+ * @description This file specifies the directories and file types to be included in the asset pipeline.
+ * It links images, stylesheets, JavaScript files, and other assets to be precompiled and served.
+ */
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../builds
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+# CSS や JavaScript などのファイルを管理するシステムです。デフォルトでは、Rails は決まった場所（stylesheets など）のファイルしか探しません。
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
