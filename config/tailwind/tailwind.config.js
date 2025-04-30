5d0d59d7ef1b5aef0f7376e9a0181fa99750fe4b
ttthot <softbasoftb@gmail.com>
2025-04-30 06:58:08 +0000

    余分な設定をすべて削除

---
+++ config/tailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  corePlugins: {
+    preflight: true,
+  },
+  plugins: [
+  ]
+}
