b61aa5614743488e0db0395661be675c07ea83e5
ttthot <softbasoftb@gmail.com>
2025-05-02 07:01:19 +0000

    tailwindv4対応、bin/devでサーバー立ち上げに変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,8 @@
-module.exports = {
+export default {
   content: [
     "./app/views/**/*.{html,html.erb,erb}",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/assets/stylesheets/**/*.css"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'custom-blue': '#1fb6ff',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [
-    require("daisyui")
-  ],
-  daisyui: {
-    themes: ["light", "dark", "cupcake"],
-    darkTheme: "dark",
-    base: true,
-    styled: true,
-    utils: true,
-    logs: true,
-  }
+  ]
 }
