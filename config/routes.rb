45e0e8534d4c10c314fed2a1461e857600250553
ttthot <68436657+ttthot@users.noreply.github.com>
2025-05-20 14:04:57 +0000

    コメント追記、削除

--- config/routes.rb
+++ config/routes.rb
@@ -12,28 +12,24 @@
 # ルートパス（"/"）はデフォルトではコメントアウトされています。
 # デフォルトのランディングページを定義するには、`root` の行をアンコメントして修正してください。
 Rails.application.routes.draw do
+  # アプリケーションのルートURL（"/"）にアクセスしたとき、sessionsコントローラーのnewアクションを表示する
   root "sessions#new"
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/create"
 
+  # ユーザー登録用のカスタムルート
   get "/signup", to: "users#new"
 
+  # ユーザーログイン用のカスタムルート
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-
   # localhost:3000/posts
   # localhost:3000/posts/new
   resources :posts, only: %i[index new create]
   resources :users, only: %i[index show new create]
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 end
